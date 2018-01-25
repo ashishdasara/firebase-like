c2946e54cfc6995fa82392d4c478719e6f2a169f
ashishdasara <ashish.dasara@yahoo.in>
2018-01-25 21:58:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import './App.css';
+import fire from './fire';
+import CreateComment from './CreateComment'
+
+class DisplayAll extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      comments: [],
+      items: []
+    }
+  }
+  componentWillMount() {
+    this.setState({comments: this.props.comments});
+  }
+  render() {
+    return (
+      <div className="display_all">
+        {this.state.comments.length!==0? this.state.comments.map(comment => {
+            return(<DisplayComment key={comment.id} comment={comment}/>);
+          }):null
+        }
+      </div>
+    );
+  }
+}
+
+class DisplayComment extends React.Component {
+  render() {
+    return(
+      <div className="container display_comment">
+        <p>{this.props.comment.text}</p>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      comments: [],
+      items: []
+    }
+  }
+  
+  componentWillMount() {
+    let newComments=this.state.comments;
+    this.firebaseRef = fire.database().ref("comments");
+    this.firebaseRef.on("child_added", function(dataSnapshot) {
+      newComments.push(dataSnapshot.val());
+      this.setState({comments: newComments});
+    }.bind(this));
+    console.log(this.state.comments)
+  }
+
+  render() {
+    return(
+      <div className="container">
+        <CreateComment comments={this.state.comments}/>
+        <DisplayAll comments={this.state.comments}/>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/CreateComment.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import fire from './fire';
+
+class CreateComment extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      comment: {
+        text: "",
+        id: 0,
+        properties: {
+          likes: 0,
+          shares: 0
+        }
+      }
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+
+  }
+  componentWillReceiveProps(nextProps) {
+    this.forceUpdate();
+    var length= this.props.comments.length;
+    var newComment=this.state.comment;
+    newComment.id = length;
+    this.setState({comment: newComment});
+  }
+
+  handleClick=(e)=> {
+
+    e.preventDefault();
+    this.firebaseRef = fire.database().ref("comments");
+    this.firebaseRef.child(this.state.comment.id).set(this.state.comment
+    );
+    var newComment=this.state.comment;
+    newComment.text = "";
+    this.setState({comment: newComment});
+  }
+
+  handleTextChange = (e) => {
+    var newComment=this.state.comment;
+    newComment.text = e.target.value;
+    this.setState({comment: newComment});
+  }
+  render() {
+    return(
+        <div className="create_comment">
+          <div className="container header_box">
+            <h1>MiniBlog</h1>
+          </div>
+          <div className="container form_area">
+            <textarea value={this.state.comment.text} initial-value={this.state.comment.text} onChange={this.handleTextChange} placeholder="What's on your mind?"></textarea>
+            <button onClick={this.handleClick}>Post</button>
+          </div>
+        </div>
+    )
+  }
+}
+
+export default CreateComment
