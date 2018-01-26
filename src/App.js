faef9b8e03b7cadcc4bf7cb9d0248a6cbd9d4086
ashishdasara <ashish.dasara@yahoo.in>
2018-01-26 09:44:40 +0530

    6

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class DisplayComment extends React.Component {
       <div className="container display_comment">
         <p>{this.props.comment.text}</p>
         <FacebookProvider appId="334164410431105">
-        <Share href="#" quote={this.props.comment.text}>
+        <Share quote={this.props.comment.text}>
           <button type="button">Share</button>
         </Share>
       </FacebookProvider>
